8a31eddb9eeb4944f09c7f2cdc515bdbea8c9e4e
Tainan <tainan_abatti@gmail.com>
2019-05-06 21:44:54 -0300

    segundo

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -20,10 +20,39 @@ class ProdutoController extends Controller
     public function store(Request $request){
         $product = new Produto;
         $product->nome = $request->nome;
-        $product->descricao = $request->descri;
-        $product->name = $request->name;
-        $product->name = $request->name;
-        $product->name = $request->name;
+        $product->descricao = $request->descricao;
+        $product->preco = $request->preco;
+        $product->save();
+
+        return redirect()->route('product.index')->with('message', 'Produto criado com sucesso');
 
     }
+
+    public function show($id){
+        //
+    }
+
+    public function edit($id){
+        $product =Produto::findOrFail($id);
+        return view('alter-produto', compact('product'));
+    }
+
+    public function update(Request $request, $id){
+        $product = Produto::findOrFail($id);
+        $product->nome = $request->nome;
+        $product->descricao = $request->descricao;
+        $product->preco = $request->preco;
+        $product->save();
+
+        return redirect()->route('product.index')->with('message', 'Produto alterado com sucesso');
+    }
+
+    public function destroy($id){
+        $product = Produto::findOrFail($id);
+        $product->delete();
+        return redirect()->route('product.index')->with('message', 'Produto excluido com sucesso');
+
+    }
+
+
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,59 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Comercial Laravel</title>
 
-        <title>Laravel</title>
+    <!-- Favicon -->
+    <link href="{{URL::asset('img/favicon.ico')}}" rel="shortcut icon">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link href="{{URL::asset('css/style-home.css')}}" rel="stylesheet" type="text/css" />
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
+    <!-- JavaScript -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="{{URL::asset('js/ajax.js')}}"></script>
+</head>
+<body>
+<div class="flex-center position-ref full-height" id="fundo">
+    @if (Route::has('login'))
+        <div class="top-right links">
+            @if (Auth::check())
+                <a href="{{ url('/home') }}">Home</a>
+            @else
+                <a href="{{ url('/login') }}">Login</a>
+                <a href="{{ url('/register') }}">Register</a>
             @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
         </div>
-    </body>
+    @endif
+    <div class="content">
+        <i class="fa fa-bank"></i>
+        <div class="title m-b-md">
+            Comercial Laravel
+        </div>
+        <div class="links">
+            <a href="">
+                <i class="fa fa-file-text"> Documentação</i>
+            </a>
+            <a href="">
+                <i class="fa fa-youtube-play"> Youtube</i>
+            </a>
+            <a href="">
+                <i class="fa fa-github"> GitLab</i>
+            </a>
+            <a href="">
+                <i class="fa fa-info"> Sobre</i>
+            </a>
+        </div>
+    </div>
+</div>
+</body>
 </html>
